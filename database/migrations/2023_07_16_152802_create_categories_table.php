25bbf325272c52c72f5ba8a9e9c692d45a43967a
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-07-16 18:52:05 +0200

    migration Categories

--- database/migrations/2023_07_16_152802_create_categories_table.php
+++ database/migrations/2023_07_16_152802_create_categories_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 50);
+            $table->text('description');
         });
     }
 
---
+++ database/seeders/CategoriesTableseeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategoriesTableseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (config('categories') as $objCategory) {
+            Category::create($objCategory);
+        }
+    }
+}
